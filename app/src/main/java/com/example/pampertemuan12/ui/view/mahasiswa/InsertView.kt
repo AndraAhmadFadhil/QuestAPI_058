f07e999f46adfd9b76bf98fa0614b7dcb490fe4e
Ann <andra.ahmad.ft22@mail.umy.ac.id>
2024-12-30 05:36:35 +0700

    menambahkan variabel pada fun EntryMhsScreen untuk meluncurkan coroutine secara aman di dalam konteks Compose

--- app/src/main/java/com/example/pampertemuan12/ui/view/mahasiswa/InsertView.kt
+++ app/src/main/java/com/example/pampertemuan12/ui/view/mahasiswa/InsertView.kt
@@ -1,6 +1,12 @@
 package com.example.pampertemuan12.ui.view.mahasiswa
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pampertemuan12.ui.navigation.DestinasiNavigasi
+import com.example.pampertemuan12.ui.viewmodel.InsertViewModel
 
 object DestinasiEntry: DestinasiNavigasi {
     override val route= "item_entry"
@@ -8,3 +14,12 @@ object DestinasiEntry: DestinasiNavigasi {
 
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryMhsScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+}
