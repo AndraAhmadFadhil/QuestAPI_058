e84fbcb90e62d509b5992fc95a51a8b9bfb6f424
Ann <andra.ahmad.ft22@mail.umy.ac.id>
2024-12-30 05:47:11 +0700

    membangun tampilan dengan struktur Scaffold

--- app/src/main/java/com/example/pampertemuan12/ui/view/mahasiswa/InsertView.kt
+++ app/src/main/java/com/example/pampertemuan12/ui/view/mahasiswa/InsertView.kt
@@ -1,13 +1,21 @@
 package com.example.pampertemuan12.ui.view.mahasiswa
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pampertemuan12.ui.customwidget.CostumeTopAppBar
 import com.example.pampertemuan12.ui.navigation.DestinasiNavigasi
 import com.example.pampertemuan12.ui.viewmodel.InsertViewModel
+import kotlinx.coroutines.launch
 
 object DestinasiEntry: DestinasiNavigasi {
     override val route= "item_entry"
@@ -24,4 +32,32 @@ fun EntryMhsScreen(
 ) {
     val coroutineScope = rememberCoroutineScope()
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiEntry.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ) {innerPadding ->
+        EntryBody(
+            insertUiState = viewModel.uiState,
+            onSiswaValueChange = viewModel::updateInsertMhsState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertMhs()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+
+    }
 }
