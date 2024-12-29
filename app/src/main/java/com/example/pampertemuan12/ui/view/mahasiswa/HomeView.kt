d64efc74be15d1306d839ee64ddc1c12be3025d8
Ann <andra.ahmad.ft22@mail.umy.ac.id>
2024-12-30 06:33:27 +0700

    menambahkan fungsi untuk menampilkan daftar mahasiswa dalam bentuk kartu yang dapat diklik untuk melihat detail dan tombol untuk menghapus data

--- app/src/main/java/com/example/pampertemuan12/ui/view/mahasiswa/HomeView.kt
+++ app/src/main/java/com/example/pampertemuan12/ui/view/mahasiswa/HomeView.kt
@@ -1,10 +1,15 @@
 package com.example.pampertemuan12.ui.view.mahasiswa
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Card
@@ -18,6 +23,32 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.pampertemuan12.data.Mahasiswa
+
+@Composable
+fun MhsLayout(
+    mahasiswa: List<Mahasiswa>,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Mahasiswa) -> Unit,
+    onDeleteClick: (Mahasiswa) -> Unit = {}
+){
+    LazyColumn(
+        modifier = modifier,
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(mahasiswa){ mahasiswa ->
+            MhsCard(
+                mahasiswa = mahasiswa,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { onDetailClick(mahasiswa) },
+                onDeleteClick = {
+                    onDeleteClick(mahasiswa)
+                }
+            )
+        }
+    }
+}
 
 @Composable
 fun MhsCard(
