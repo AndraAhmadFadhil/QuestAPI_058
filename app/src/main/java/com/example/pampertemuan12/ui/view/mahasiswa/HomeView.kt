784907b789429e9c44a7ea9bc4b6e48a624833db
Ann <andra.ahmad.ft22@mail.umy.ac.id>
2024-12-30 06:36:54 +0700

    membuat fungsi untuk menampilkan pesan kesalahan saat terjadi masalah

--- app/src/main/java/com/example/pampertemuan12/ui/view/mahasiswa/HomeView.kt
+++ app/src/main/java/com/example/pampertemuan12/ui/view/mahasiswa/HomeView.kt
@@ -1,5 +1,6 @@
 package com.example.pampertemuan12.ui.view.mahasiswa
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -12,6 +13,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
@@ -21,8 +23,26 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.example.pampertemuan12.R
 import com.example.pampertemuan12.data.Mahasiswa
+
+@Composable
+fun OnError(retryAction: () -> Unit, modifier: Modifier = Modifier){
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(painter = painterResource(id = R.drawable.network_error), contentDescription = "")
+        Text(text = stringResource(id = R.string.loading_failed), modifier = Modifier.padding(16.dp))
+        Button(onClick = retryAction) {
+            Text(stringResource(id = R.string.retry))
+        }
+    }
+}
 
 @Composable
 fun MhsLayout(
