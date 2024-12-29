5731320fa462bbbd86aaad942a99bd4de9f7ceee
Ann <andra.ahmad.ft22@mail.umy.ac.id>
2024-12-30 04:39:44 +0700

    menambahkan function untuk memetakan data antara domain model (Mahasiswa) dan struktur yang digunakan untuk UI

--- app/src/main/java/com/example/pampertemuan12/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pampertemuan12/ui/viewmodel/InsertViewModel.kt
@@ -2,6 +2,19 @@ package com.example.pampertemuan12.ui.viewmodel
 
 import com.example.pampertemuan12.data.Mahasiswa
 
+fun InsertUiEvent.toMhs(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
+
+fun Mahasiswa.toUiStateMhs(): InsertUiState = InsertUiState(
+    insertUiEvent = toInsertUiEvent()
+)
+
 fun Mahasiswa.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
     nim = nim,
     nama = nama,
