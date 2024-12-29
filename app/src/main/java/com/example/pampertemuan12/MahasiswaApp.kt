4363a1a6c19707d27c6765292a0569cf6cadddb4
Ann <andra.ahmad.ft22@mail.umy.ac.id>
2024-12-30 04:30:24 +0700

    menambahkan MahasiswaApp untuk mendukung manajemen dependencies secara terpusat di aplikasi Android

---
+++ app/src/main/java/com/example/pampertemuan12/MahasiswaApp.kt
@@ -0,0 +1,14 @@
+package com.example.pampertemuan12
+
+import android.app.Application
+import com.example.pampertemuan12.dependenciesinjection.AppContainer
+import com.example.pampertemuan12.dependenciesinjection.MahasiswaContainer
+
+class MahasiswaApp: Application() {
+
+    lateinit var container: AppContainer
+    override fun onCreate() {
+        super.onCreate()
+        container = MahasiswaContainer()
+    }
+}
