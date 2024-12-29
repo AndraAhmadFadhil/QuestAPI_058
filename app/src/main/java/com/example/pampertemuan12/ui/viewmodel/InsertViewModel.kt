841a24a17f3beab1e543e3eb9bdfb5ee2f2164dc
Ann <andra.ahmad.ft22@mail.umy.ac.id>
2024-12-30 04:37:11 +0700

    menambahkan fungsi untuk mengkonversi data dari model domain ke event UI

---
+++ app/src/main/java/com/example/pampertemuan12/ui/viewmodel/InsertViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.pampertemuan12.ui.viewmodel
+
+import com.example.pampertemuan12.data.Mahasiswa
+
+fun Mahasiswa.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
