3a12e39cff74c7e16149a5e085c00755f3b06e4f
Ann <andra.ahmad.ft22@mail.umy.ac.id>
2024-12-30 04:49:36 +0700

    menambahkan data class untuk mempermudah pengelolaan data input dan status UI

--- app/src/main/java/com/example/pampertemuan12/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pampertemuan12/ui/viewmodel/InsertViewModel.kt
@@ -2,6 +2,19 @@ package com.example.pampertemuan12.ui.viewmodel
 
 import com.example.pampertemuan12.data.Mahasiswa
 
+data class InsertUiState(
+    val insertUiEvent: InsertUiEvent = InsertUiEvent()
+)
+
+data class InsertUiEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val jenisKelamin: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
+
 fun InsertUiEvent.toMhs(): Mahasiswa = Mahasiswa(
     nim = nim,
     nama = nama,
