ea6951369985a0805993e7d938d8f84cff77907c
Ann <andra.ahmad.ft22@mail.umy.ac.id>
2024-12-30 05:28:43 +0700

    menambahkan DestinasiEntry pada InsertView.kt untuk mendefinisikan rute dan judul untuk tampilan entri mahasiswa dalam aplikasi

---
+++ app/src/main/java/com/example/pampertemuan12/ui/view/mahasiswa/InsertView.kt
@@ -0,0 +1,10 @@
+package com.example.pampertemuan12.ui.view.mahasiswa
+
+import com.example.pampertemuan12.ui.navigation.DestinasiNavigasi
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route= "item_entry"
+    override val titleRes = "Entry Mhs"
+
+}
+
