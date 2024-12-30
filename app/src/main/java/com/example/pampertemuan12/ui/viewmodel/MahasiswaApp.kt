0791268beb99bac01a1eab8f892ef54dabaf6e36
Ann <andra.ahmad.ft22@mail.umy.ac.id>
2024-12-30 12:04:50 +0700

    menambahkan modifier pada MahasiswaApp

--- app/src/main/java/com/example/pampertemuan12/ui/viewmodel/MahasiswaApp.kt
+++ app/src/main/java/com/example/pampertemuan12/ui/viewmodel/MahasiswaApp.kt
@@ -7,18 +7,18 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
-import com.example.pampertemuan12.data.Mahasiswa
 import com.example.pampertemuan12.ui.navigation.PengelolaHalaman
 
 @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MahasiswaApp() {
+fun MahasiswaApp(
+    modifier: Modifier = Modifier
+) {
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
     Scaffold(
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
