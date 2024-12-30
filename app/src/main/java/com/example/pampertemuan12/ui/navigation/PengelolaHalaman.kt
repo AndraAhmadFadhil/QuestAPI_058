4c1db22cda06fdd7d90c522ab7b98c6897cca568
Ann <andra.ahmad.ft22@mail.umy.ac.id>
2024-12-30 12:07:02 +0700

    menambahkan destinasi detail dan destinasi update pada PengelolaHalaman

--- app/src/main/java/com/example/pampertemuan12/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pampertemuan12/ui/navigation/PengelolaHalaman.kt
@@ -5,13 +5,19 @@ import androidx.annotation.RequiresExtension
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.pampertemuan12.ui.view.mahasiswa.DestinasiDetail
 import com.example.pampertemuan12.ui.view.mahasiswa.DestinasiEntry
 import com.example.pampertemuan12.ui.view.mahasiswa.DestinasiHome
+import com.example.pampertemuan12.ui.view.mahasiswa.DestinasiUpdate
+import com.example.pampertemuan12.ui.view.mahasiswa.DetailScreen
 import com.example.pampertemuan12.ui.view.mahasiswa.EntryMhsScreen
 import com.example.pampertemuan12.ui.view.mahasiswa.HomeScreen
+import com.example.pampertemuan12.ui.view.mahasiswa.UpdateScreen
 
 @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
 @Composable
@@ -19,25 +25,66 @@ fun PengelolaHalaman(navController: NavHostController = rememberNavController())
     NavHost(
         navController = navController,
         startDestination = DestinasiHome.route,
-        modifier = Modifier
-    ){
-        composable(DestinasiHome.route){
+        modifier = Modifier,
+    ) {
+        composable(DestinasiHome.route) {
             HomeScreen(
-                navigateToltemEntry = { navController.navigate(DestinasiEntry.route) },
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
                 onDetailClick = {
-
+                    navController.navigate("${DestinasiDetail.route}/$it")
                 }
             )
         }
-
-        composable(DestinasiEntry.route){
+        composable(DestinasiEntry.route) {
             EntryMhsScreen(navigateBack = {
-                navController.navigate(DestinasiHome.route){
-                    popUpTo(DestinasiHome.route){
+                navController.navigate(DestinasiHome.route) {
+                    popUpTo(DestinasiHome.route) {
                         inclusive = true
                     }
                 }
             })
+        }
+        composable(
+            DestinasiDetail.routeWithArgs,
+            arguments = listOf(
+                navArgument(DestinasiDetail.NIM) {
+                    type = NavType.StringType
+                }
+            )
+        ) {
+            val nim = it.arguments?.getString(DestinasiDetail.NIM)
+            nim?.let { nim ->
+                DetailScreen(
+                    onBackClick = {
+                        navController.popBackStack()
+                    },
+                    onEditClick = {
+                        navController.navigate("${DestinasiUpdate.route}/$it")
+                    },
+                    nim = nim,
+                    onDeleteClick = {
+                        navController.popBackStack()
+                    }
+                )
+            }
+        }
+        composable(
+            route = DestinasiUpdate.routesWithArg,
+            arguments = listOf(
+                navArgument(DestinasiUpdate.NIM) { type = NavType.StringType }
+            )
+        ) { backStackEntry ->
+            val nim = backStackEntry.arguments?.getString(DestinasiUpdate.NIM)
+            nim?.let {
+                UpdateScreen(
+                    onBack = { navController.popBackStack() },
+                    onNavigate = {
+                        navController.navigate(DestinasiHome.route) {
+                            popUpTo(DestinasiHome.route) { inclusive = true }
+                        }
+                    },
+                )
+            }
         }
     }
 }
