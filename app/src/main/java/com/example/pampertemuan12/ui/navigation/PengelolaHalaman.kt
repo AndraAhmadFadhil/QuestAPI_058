5fcbcd569c4af1cf9613e0d01788691621311faf
Ann <andra.ahmad.ft22@mail.umy.ac.id>
2024-12-30 07:19:33 +0700

    membuat fungsi PengelolaHalaman untuk mengatur navigasi antar layar HomeScreen dan EntryMhsScreen menggunakan NavHostController

---
+++ app/src/main/java/com/example/pampertemuan12/ui/navigation/PengelolaHalaman.kt
@@ -0,0 +1,43 @@
+package com.example.pampertemuan12.ui.navigation
+
+import android.os.Build
+import androidx.annotation.RequiresExtension
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.pampertemuan12.ui.view.mahasiswa.DestinasiEntry
+import com.example.pampertemuan12.ui.view.mahasiswa.DestinasiHome
+import com.example.pampertemuan12.ui.view.mahasiswa.EntryMhsScreen
+import com.example.pampertemuan12.ui.view.mahasiswa.HomeScreen
+
+@RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+@Composable
+fun PengelolaHalaman(navController: NavHostController = rememberNavController()){
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ){
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToltemEntry = { navController.navigate(DestinasiEntry.route) },
+                onDetailClick = {
+
+                }
+            )
+        }
+
+        composable(DestinasiEntry.route){
+            EntryMhsScreen(navigateBack = {
+                navController.navigate(DestinasiHome.route){
+                    popUpTo(DestinasiHome.route){
+                        inclusive = true
+                    }
+                }
+            })
+        }
+    }
+}
